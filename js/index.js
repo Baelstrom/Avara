424c87bcad1777cddbc9c2128d667df205c95ae2
Mohamed Ikhwan <1khwan.m0@gmail.com>
2018-02-28 02:09:37 +0800

    Add Stagger to Menu Items

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 let menuToggle = true;
 
 function toggleMenu() {
+  toggleMenuIcon()
+  staggerMenuItems()
+}
+
+function toggleMenuIcon () {
   // check current state
   // if menu is shown then hide Hamburger display X
   // else hide X show Hamburger
@@ -16,15 +21,24 @@ function toggleMenu() {
     // hide X
     TweenLite.to(iconCloseBlack, 0.2, {opacity: 0})
     // show Ham
-    TweenLite.to(iconHamWhite, 0.2, {opacity: 1})
+    TweenLite.to(iconHamWhite, 0.2, {opacity: 1}).delay(0.5)
   }
-
 
   // toggle state
   menuToggle = !menuToggle
 }
 
+function staggerMenuItems () {
+  // let menuItems = getByClassName('sideMenuList').childNodes
+  let menuItems = getByClassName('sideMenuList')[0].childNodes
 
+
+  if(menuToggle) {
+    TweenLite.to(menuItems, 0.2, {opacity: 0})
+  } else {
+    TweenMax.staggerFrom(menuItems, 0.3, {opacity:0,x:-300},0.05)
+  }
+}
 
 // Helper Functions
 
@@ -43,5 +57,5 @@ function getById ( targetId ) {
 
 // Abstracted get by classname function
 function getByClassName ( targetClass ) {
-  return document.getElementByClassName(targetClass)
+  return document.getElementsByClassName(targetClass)
 }
