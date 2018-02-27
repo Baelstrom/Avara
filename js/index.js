24afbba2967a3ef5badb726e8863cb6b07493640
Mohamed Ikhwan <1khwan.m0@gmail.com>
2018-02-28 05:33:05 +0800

    add Intro Animations
    -- chevrons still incomplete

--- js/index.js
+++ js/index.js
@@ -1,8 +1,51 @@
 let menuToggle = true;
 
+function intro() {
+  //get variables
+  let mainHeading = getById('mainHeading')
+  let subHeading = getById('subHeading')
+  let introListItems = getByClassName('introList')[0].childNodes
+  var introListItemArray = [];
+  for(var i = introListItems.length; i--; introListItemArray.unshift(introListItems[i]));
+  introListItems = introListItemArray.filter( item => item.className === 'introListItem')
+  let skipToFav = getByClassName('skipToFav')[0]
+  let scrollIconGroup = getByClassName('scrollIconGroup')[0]
+  let chevron1 = getByClassName('chevron1')[0]
+  let chevron2 = getByClassName('chevron2')[0]
+  let chevron3 = getByClassName('chevron3')[0]
+  let chevron4 = getByClassName('chevron4')[0]
+  let chevron5 = getByClassName('chevron5')[0]
+
+  //
+  var introTimeline = new TimelineMax()
+  var introListTimeLine = new TimelineLite()
+  introTimeline.add(TweenLite.from(mainHeading, 1.5, {opacity:0, y:100,ease:Power3.easeInOut}))
+  introTimeline.add(TweenLite.from(subHeading, 1.5, {opacity:0, y:100,ease:Power3.easeInOut}).delay(0.3))
+
+
+  introListItems.forEach( item => {
+    introTimeline.add([
+      TweenLite.from(item.children[0], 1, {opacity:0, x:-100}),
+      TweenLite.from(item.children[1], 1, {opacity:0}),
+      TweenLite.from(item.children[2], 1, {opacity:0, x:100})
+    ]).delay(1)
+  })
+  introTimeline.add( TweenLite.from(skipToFav, 1.5, {opacity:0, y:100,ease:Power3.easeInOut}))
+  introTimeline.add([
+    TweenLite.from(scrollIconGroup, 1.5, {opacity:0, ease:Power3.easeInOut}),
+    // TweenLite.to(chevron1, 1.5, {top:72, opacity:0, ease:Power3.easeIn, repeat: 10}),
+    // TweenLite.to(chevron2, 1.5, {top:52, opacity:0.25, ease:Power3.easeIn, repeat: 10}).repeat(-1),
+
+  ])
+  // TweenLite.from(mainHeading, 1.5, {opacity:0, y:100,ease:Power3.easeInOut})
+}
+
+intro()
+
 function toggleMenu() {
   toggleMenuIcon()
   staggerMenuItems()
+
 }
 
 function toggleMenuIcon () {
