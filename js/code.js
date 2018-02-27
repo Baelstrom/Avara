b79896be34399ba37b9c3cac213e573a0be6deae
Mohamed Ikhwan <1khwan.m0@gmail.com>
2018-02-27 18:33:30 +0800

    Chevrons Ready
    - This commit is long overdue, i honestly just forgot about git for a while
    
    Let's  see if I can run down everything:
    
    - Created base HTML structure
    - Worked on Side panel scroll - in 
    - Fixed up Chevrons
    - Added in fonts

---
+++ js/code.js
@@ -0,0 +1,62 @@
+// Global Variables
+let scale = 1;
+let currentScene = 0;
+
+let scenes = [
+  {
+    id: 1,
+    name: 'landingPage',
+    // totalTime: 0s,
+    // timeScale
+    elements: [
+
+    ],
+  }
+]
+
+
+// Helper Functions for Scenes
+
+function loadScene ( sceneName ) {
+  // load defined Scene
+}
+
+function clearScene () {
+  // clear current Scene
+}
+
+function nextScreen () {
+  // loads next scene
+  // clears current Scene
+  // starts the next Scene
+}
+
+
+
+
+
+// Helper functions for Audio
+
+function speak () {
+
+}
+
+function mute () {
+
+}
+
+function unmute () {
+
+}
+
+function asyncFetchAudio () {
+
+}
+
+
+
+
+
+
+
+let currentState = ''
